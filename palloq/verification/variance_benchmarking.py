ac4a2bdd2936436a467c0ea63aa315744db34086
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2021-05-30 15:51:43 +0900

    update verification functions

--- palloq/verification/variance_benchmarking.py
+++ palloq/verification/variance_benchmarking.py
@@ -1,27 +1,66 @@
-from typing import List, Union
+from typing import List, Tuple
 
 import qiskit.ignis.verification.randomized_benchmarking as rb
 from qiskit.providers.ibmq.ibmqbackend import IBMQBackend
 from qiskit.providers.ibmq.job.ibmqjob import IBMQJob
+from qiskit.compiler import transpile
 
-from palloq.utils import get_IBMQ_backend
 
-
-def run(backend):
+def run_rb(
+    backend,
+    shots,
+    repeat: int,
+    length_vector: List[int] = [1, 10, 20, 50, 75, 100, 125, 150, 175, 200],
+    nseeds: int = 1,
+):
     """
     Args:
         backend: Backend device you want to run
 
+    The roll of `nseed` and `repeat` is same (rb_seeds).
+    But in this function, `nseed` is used to create set of rb
+    jobs of one target qubit(s).
+    Instead of that, `repeat` is used to create set of jobs of whole
+    sets of target qubits, and then repeat to create and send.
+    e.g.
+        `nseeds = n`
+            _jobs_1 = [job_1-1, job_1-2, job_1-3, ..., job_1-n]
+            ...
+            _jobs_m = [job_m-1, job_m-2, job_m-3, ..., job_m-n]
 
+            then,
+
+            _jobssets = [_jobsr_1, ..., _jobs_m]
+
+
+        `repeat = n`
+            _jobsets_1 = [[job_1-1], [job_2-1], [job_3-1], ..., [job_m-1]]
+            ...
+            _jobsets_n = [[job_1-n], [job_2-n], [job_3-n], ..., [job_m-n]]
+
+        In both cases, `job_i-j` object is `Tuple[IBMQJob, List[gpc]]`.
+        `gpc` is gate per clifford represented as dict.
+        `i` means number of patterns of target qubits.
+        `j` means rb_seeds
+
+        In general, the characteristics of hardware error is varing in time,
+        so I prefer to use `nseeds = 1` and `repeat = n`, `n` is
+        the rb_seeds you want to set in this case.
     """
-    # prepare backend device
-    get_IBMQ_backend()
 
     # prepare rb pattern based on backend device.
 
     # run simRB
 
     # run individual RB
+    for i in range(repeat):
+        _run_RB(
+            backend,
+            shots,
+            length_vector,
+            nseeds,
+            rb_pattern,
+        )
 
     # run simRB again
 
@@ -70,7 +109,7 @@ def _run_RB(
     length_vector: List[int],
     nseeds: int,
     rb_pattern: List[List[int]],
-) -> Union[IBMQJob, List[IBMQJob]]:
+) -> List[Tuple[IBMQJob, List[dict]]]:
     """Function for Randomized Benchmarking"""
     rb_opts = {
         "length_vector": length_vector,
@@ -79,22 +118,32 @@ def _run_RB(
     }
 
     rb_circs, xdata = rb.randomized_benchmarking_seq(**rb_opts)
+    basis_gates = backend.configuration().basis_gates
+    rb_circs = [transpile(_rb_circ, basis_gates=basis_gates) for _rb_circ in rb_circs]
 
+    gpcs = []
     # Calculate gate per clifford (gcp)
-    gates_per_cliff = rb.rb_utils.gates_per_clifford(
-        transpile_list_pairs,
-        xdata[0],
-        basis_gates,
-        qubits=list(twoQconnection),
-    )
+    for _rb_seq, _qubits in zip(xdata, rb_pattern):
+        gates_per_cliff = rb.rb_utils.gates_per_clifford(
+            rb_circs,
+            clifford_lengths=_rb_seq,
+            basis=basis_gates,
+            qubits=_qubits,
+        )
+    gpcs.append(gates_per_cliff)
 
-    # run rb on backend
-    job = backend.run(
-        circuits=rb_circs,
-        shots=shots,
-    )
+    jobs = []
+    for rb_seed, rb_circ_i in enumerate(rb_circs):
 
-    return job
+        # run rb on backend
+        _job = backend.run(
+            circuits=rb_circ_i,
+            shots=shots,
+        )
+        job = (_job, gpcs)
+        jobs.append(job)
+
+    return jobs
 
 
 def _simRB(rb_pattern_set: List[List[List[int]]]):
